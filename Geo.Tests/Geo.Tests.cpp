151498f97b79b4001d5efd6d8b612b7e3560d393
kovor <kovor@SINNER>
2024-09-11 14:36:07 +0300

    Добавьте файлы проекта.

---
+++ Geo.Tests/Geo.Tests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GeoTests
+{
+	TEST_CLASS(GeoTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
